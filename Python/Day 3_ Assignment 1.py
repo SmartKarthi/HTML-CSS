41a2604433a8dd04e4fef47f904eefbd80fa49b4
SmartKarthi <107244430+SmartKarthi@users.noreply.github.com>
2022-12-04 14:12:28 +0530

    Add files via upload

---
+++ Python/Day 3_ Assignment 1.py
@@ -0,0 +1,7 @@
+ft=int(input("Enter the current plane feet :"))
+if 1000<=ft and ft<=5000 :
+    print("Land Your Plane...")
+elif 5001<=ft and ft<=10000:
+    print("Ready to land your plane!!!...")
+else:
+    print("The pilot to go around now!!!...")
